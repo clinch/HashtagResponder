dfe7cb718310f679296a690bce5823dc8d9d572b
Chris Inch <github@chrisinch.com>
2015-10-13 21:58:15 -0400

    More progress and some stub ideas

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # HashtagResponder
 A Node.js application that will automatically respond to any tweets using a specific hashtag.
+
+## Requirements
+
+- This script uses [Redis](http://redis.io/) to keep track of tweets. So you need access to a Redis server. [Download the latest stable version of Redis](http://redis.io/).
+- You will need access tokens to the Twitter API. Get started by creating an app at the [Twitter Dev Site](https://dev.twitter.com).
+
+## Getting started
+
+- There is a sample config file found at **config.json.sample**. Start by copying this to **config.json** and then edit the values found in this file to suit your needs.
--- hashtag-responder.js
+++ hashtag-responder.js
@@ -3,10 +3,23 @@
 var twitterAPI = require('node-twitter-api'),
  	fs    = require('fs'),
     nconf = require('nconf'), 
+    redis = require("redis"),
     debug = require('debug')('hashtag');
 
 const CONFIG_PATH = 'config.json';
 
+// Use MAX_TWEET_COUNT as a safety measure in case you get a huge search result
+// which starts your client responding. 
+const MAX_TWEET_COUNT = 15;
+
+const REDIS_PREFIX = "hashtag-responder:";
+
+// Set up Redis connection
+let client = redis.createClient();
+client.on("error", err => console.log("Redis Error " + err));
+
+
+// Read config file
 nconf.argv()
    .file({ file: CONFIG_PATH });
 
@@ -16,15 +29,59 @@ let twitter = new twitterAPI({
     callback: nconf.get('callback')
 });
 
+
+// Get info on currently-authenticated user
+// https://dev.twitter.com/rest/reference/get/account/settings
+
+
+// Perform the twitter search.
 twitter.search(
-	{q: nconf.get('searchQuery')}, 
+	{
+		q: nconf.get('searchQuery'),
+		result_type: 'recent',
+		include_entities: true,
+		count: MAX_TWEET_COUNT
+	}, 
 	nconf.get('accessToken'),
 	nconf.get('accessTokenSecret'),
 	function(error, data, response) {
 	    if (error) {
 	        debug("Something went wrong with tweet retrieval")
 	    } else {
-	        debug(data.statuses[0]);
+	    	respondToTweets(data.statuses);
 	    }
 	}
 );
+
+/**
+ * Analyzes the current tweet/status array, and then responds to them.
+ * @param  {Array} tweetArray An array of twitter Tweet objects.
+ *                            https://dev.twitter.com/overview/api/tweets
+ */
+function respondToTweets(tweetArray) {
+
+	let maxId = 0;
+	let screenName = '';
+
+	for (let i = 0; i < tweetArray.length; i++) {
+		// Check to make sure this isn't a tweet that we made ourselves
+		
+
+		// Check to make sure this is an original tweet and NOT a retweet.
+
+
+		screenName = `@${tweetArray[i].user.screen_name}`;
+
+		debug(screenName + ": " + tweetArray[i].text);
+		if (tweetArray[i].id > maxId) {
+			maxId = tweetArray[i].id;
+		}
+	}
+
+	// We save the max message id because we can search for messages newer than
+	// this next iteration.
+	if (maxId != 0) {
+		client.set(`${REDIS_PREFIX}LastId`, maxId);
+	}
+
+}
