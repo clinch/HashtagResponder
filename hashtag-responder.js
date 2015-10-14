124addfd134ed00589445de34df99fc9552cd611
Chris Inch <github@chrisinch.com>
2015-10-13 20:48:26 -0400

    Updated the hashtag responder to read search query from config.

--- hashtag-responder.js
+++ hashtag-responder.js
@@ -5,12 +5,8 @@ var twitterAPI = require('node-twitter-api'),
     nconf = require('nconf'), 
     debug = require('debug')('hashtag');
 
-debug('Starting...');
-
 nconf.argv()
    .file({ file: 'config.json' });
-   
-debug('Got config values. Eg key: ' + nconf.get('consumerKey'));
 
 let twitter = new twitterAPI({
     consumerKey: nconf.get('consumerKey'),
@@ -18,8 +14,8 @@ let twitter = new twitterAPI({
     callback: nconf.get('callback')
 });
 
-twitter.search(//"tweets", 
-	{q: "#hansie"}, 
+twitter.search
+	{q: nconf.get('searchQuery')}, 
 	nconf.get('accessToken'),
 	nconf.get('accessTokenSecret'),
 	function(error, data, response) {
